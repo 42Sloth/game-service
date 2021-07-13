590efb5f79e08c48ab87888e60d7d9e6518f2598
nonalias <ktw1532@naver.com>
2021-07-13 16:51:16 +0900

    initialize

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Game from './Game';
+import Chat from './Chat';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      {/* <Chat/> */}
+      <Game />
+    </div>
+  );
+}
+
+export default App;
