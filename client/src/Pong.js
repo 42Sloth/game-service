1cf62209947bf181ea509217627f63baec10411e
LeeJuhae <tlatmsdlwngo@naver.com>
2021-07-14 15:49:54 +0900

    FEAT: issue/#1 .env 파일 생성

--- client/src/Pong.js
+++ client/src/Pong.js
@@ -1,24 +1,29 @@
 import { useEffect } from 'react';
 import io from 'socket.io-client';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const ip = process.env.REACT_APP_GAME_SOCKET_IP;
+const port = process.env.REACT_APP_GAME_SOCKET_PORT;
 
 const Pong = () => {
-  const socket = io('ws://localhost:81/pong');
+  console.log(ip, port);
+  const socket = io(`ws://${ip}:${port}/pong`);
   let nickname = '';
 
   let canvas = null;
   let context = null;
 
-  const keydown = (e) => {
-    socket.emit('key-action', { player: nickname, type: 'down', keyCode: e.keyCode});
+  const keydown = e => {
+    socket.emit('key-action', { player: nickname, type: 'down', keyCode: e.keyCode });
   };
 
-  const keyup = (e) => {
-    socket.emit('key-action', { player: nickname, type: 'up', keyCode: e.keyCode});
-
+  const keyup = e => {
+    socket.emit('key-action', { player: nickname, type: 'up', keyCode: e.keyCode });
   };
 
   const init = () => {
-    console.log('init')
+    console.log('init');
     document.addEventListener('keydown', keydown);
     document.addEventListener('keyup', keyup);
   };
@@ -36,8 +41,8 @@ const Pong = () => {
       gameState.player_left.x,
       gameState.player_left.y,
       gameState.player_left.width,
-      gameState.player_left.height      
-    )
+      gameState.player_left.height
+    );
     // draw player2
     context.fillRect(
       gameState.player_right.x,
@@ -47,7 +52,12 @@ const Pong = () => {
     );
 
     // draw ball
-      context.fillRect(gameState.ball.x, gameState.ball.y, gameState.ball.width, gameState.ball.height)
+    context.fillRect(
+      gameState.ball.x,
+      gameState.ball.y,
+      gameState.ball.width,
+      gameState.ball.height
+    );
 
     // draw net
     context.beginPath();
@@ -74,11 +84,7 @@ const Pong = () => {
     context.font = '40px Courier';
 
     // draw the current round number
-    context.fillText(
-      gameState.round,
-      canvas.width / 2,
-      100
-    );    
+    context.fillText(gameState.round, canvas.width / 2, 100);
   };
 
   const drawGame = gameState => {
@@ -104,7 +110,7 @@ const Pong = () => {
     socket.on('init', init);
     socket.on('drawGame', drawGame);
     socket.on('start', () => {
-      console.log('start complete')
+      console.log('start complete');
     });
   }, []);
 
