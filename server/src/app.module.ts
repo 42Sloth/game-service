3a922052253bf4b1c89484c8599c1795bbec4418
nonalias <ktw1532@naver.com>
2021-07-16 16:51:50 +0900

    FEAT: #10 dockerizing database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
-
-import { PongGateway } from './modules/pong/pong.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PongModule } from './modules/pong/pong.module';
-import { PongService } from './modules/pong/pong.service';
+import { Game } from './modules/entity/Game.entity';
 
 @Module({
-  imports: [PongModule],
+  imports: [PongModule, TypeOrmModule.forRootAsync({
+      useFactory: async () => ({
+        type: 'postgres',
+        host: process.env.POSTGRES_IP,
+        port: +process.env.POSTGRES_PORT,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        autoLoadEntities: true,
+        entities: [Game],
+        synchronize: true,
+        logging:true
+      }),
+    })],
   // providers: [ChatGateway, PongGateway],
   // providers: [PongGateway, PongService],
 })
