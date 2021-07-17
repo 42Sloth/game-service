fcc8517853f7ce7ca4979eca1f9a4f7249e34237
nonalias <ktw1532@naver.com>
2021-07-17 20:27:17 +0900

    FEAT: #14 Game Stat Controller 완성

--- server/src/modules/pong/pong.service.ts
+++ server/src/modules/pong/pong.service.ts
@@ -1,4 +1,5 @@
 import { Server, Socket } from 'socket.io';
+import { Not } from 'typeorm';
 import { GameResult } from '../entity/GameResult.entity';
 import {gameLoop, Game, DIRECTION} from './pong'
 
@@ -34,5 +35,33 @@ export class PongService {
     insertResult(game: Game) {
         const gameResult: GameResult = new GameResult(game);
         gameResult.save();
+    }
+
+    async countByUsername(username: string, type: number) : Promise<number> {
+        const gameRepository = GameResult.getRepository();
+        let [items, count] = [null, 0];
+        // type 0 : win
+        if (type === 0) {
+            [items, count] = await gameRepository.findAndCount({
+                where: [
+                { playerLeft: username , winner: username},
+                { playerRight: username, winner: username },
+            ]});
+        // type 1 : lose
+        } else if (type === 1) {
+            [items, count] = await gameRepository.findAndCount({
+                where: [
+                { playerLeft: username, winner: Not(username) },
+                { playerRight: username, winner: Not(username) },
+            ]});
+        // type 2 : all
+        } else if (type === 2) {
+            [items, count] = await gameRepository.findAndCount({
+                where: [
+                { playerLeft: username },
+                { playerRight: username },
+            ]});
+        }
+        return count;
     }
 }
